f6a042044d5f35649238b5793ad6b5a3b5184f39
kaneriya drashti <143435188+Drashtikaneriya@users.noreply.github.com>
2025-07-03 19:56:29 +0530

    Add files via upload

--- Sorting/Heap_sort.c
+++ Sorting/Heap_sort.c
@@ -1,59 +1,87 @@
-// Heap Sort without using swap function
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+#define MAX 100
+
+// Heapify function
 void heapify(int arr[], int n, int i) {
-    int largest = i;
-    int left = 2 * i + 1;
-    int right = 2 * i + 2;
+    int largest = i;         // root
+    int left = 2 * i + 1;    // left child
+    int right = 2 * i + 2;   // right child
 
     if (left < n && arr[left] > arr[largest])
         largest = left;
-    if (right < n && arr[right] > arr[largest])
-        largest = right;    
-}
-
-void heapify(int arr[], int n, int i) {
-    int largest = i;
-    int left = 2 * i + 1;
-    int right = 2 * i + 2;
-
-    if (left < n && arr[left] > arr[largest])
-        largest = left;
-
+        
     if (right < n && arr[right] > arr[largest])
         largest = right;
-
+        
     if (largest != i) {
-        arr[i] = arr[i] + arr[largest];
-        arr[largest] = arr[i] - arr[largest];
-        arr[i] = arr[i] - arr[largest];
+        int temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+
+        //  sub-tree
         heapify(arr, n, largest);
     }
 }
 
 void heapSort(int arr[], int n) {
+    // Build heap
     for (int i = n / 2 - 1; i >= 0; i--)
         heapify(arr, n, i);
 
+    
     for (int i = n - 1; i > 0; i--) {
-        arr[i] = arr[i] + arr[0];
-        arr[0] = arr[i] - arr[0];
-        arr[i] = arr[i] - arr[0];
+        // Move current root to end
+        int temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+
+        // Call max heapify on the reduced heap
         heapify(arr, i, 0);
     }
 }
 
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
-}
-
 void main() {
-    int arr[] = {12, 11, 13, 5, 6, 7};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    heapSort(arr, n);
-    printf("Sorted array: ");
-    printArray(arr, n);
+    int arr[MAX];
+    int choice;
+    FILE *file;
+    clock_t start, end;
+    double cpu_time_used;
 
+    printf("Enter 1 for Best Case\nEnter 2 for Average Case\nEnter 3 for Worst Case\n");
+    scanf("%d", &choice);
+
+    switch (choice) {
+        case 1:
+            file = fopen("best.txt", "r");
+            break;
+        case 2:
+            file = fopen("avg.txt", "r");
+            break;
+        case 3:
+            file = fopen("worst.txt", "r");
+            break;
+        default:
+            printf("Enter a valid number...!!!\n");
+            return;
+    }
+
+    if (file == NULL) {
+        printf("Error opening file.\n");
+        return;
+    }
+
+    for (int i = 0; i < MAX; i++) {
+        fscanf(file, "%d", &arr[i]);
+    }
+    fclose(file);
+
+    start = clock();
+    heapSort(arr, MAX);
+    end = clock();
+
+    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Time taken by Heap Sort: %f seconds\n", cpu_time_used);
 }
